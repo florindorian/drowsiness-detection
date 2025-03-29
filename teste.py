4ce0f5441d606854d6c30d28f580c3bd0210c85f
Florindo Rian <riancarreteiro08@gmail.com>
2025-03-29 13:45:28 -0300

    feat: P1: malha inicial

---
+++ teste.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+RESOLUCAO_X = 1280
+RESOLUCAO_Y = 720
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, RESOLUCAO_X)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, RESOLUCAO_Y)
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened(): # ou: while True:
+    sucesso, frame = cap.read()
+
+    tecla = cv2.waitKey(1)
+    if tecla == 27: # Esc
+        break
+
+    if not sucesso:
+        print('Ignorando o frame vazio da camera')
+        continue
+
+    cv2.imshow('Camera', frame) # Não colocar acento no nome da imagem!
+
+cap.release()
+cv2.destroyAllWindows()
